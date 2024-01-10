1d7ea92a45ced0c9cd8b59d203dc72dac05107fa
Luisi111 <luisbala107@gmail.com>
2024-01-10 15:59:11 +0100

    kodi

---
+++ index.js
@@ -0,0 +1,12 @@
+let fruits = {
+    molla: "kuqe",
+    banane: "verdh",
+    kumbull: "jeshile",
+}
+
+let keys = Object.keys(fruits)
+
+for (let i = 0; i < keys.length; i++) {
+    let fruta = keys[i]
+    console.log("fruta " + fruta + " ngjyra " + fruits[fruta])
+}
